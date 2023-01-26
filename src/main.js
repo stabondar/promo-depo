a84f9ec90b0ed57aa5b388223c403d8338b846f6
Stas <96624704+stabondar@users.noreply.github.com>
2023-01-26 15:14:42 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './styles/style.css'
+
+import bg from './moduls/Bg.js'
+import LocoGsap from './moduls/LocoGsap'
+
+const checkPages = () => 
+{
+    const background = new bg()
+    const scroll = new LocoGsap()
+}
+checkPages()
